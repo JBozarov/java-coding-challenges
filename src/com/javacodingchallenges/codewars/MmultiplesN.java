1010e899449e711575d379c6954f862e95ebd366
jbozarov <tech@jbozarov.com>
2021-06-07 15:29:30 -0400

    better way of solving MmultiplesN

--- src/com/javacodingchallenges/codewars/MmultiplesN.java
+++ src/com/javacodingchallenges/codewars/MmultiplesN.java
@@ -2,11 +2,13 @@ package com.javacodingchallenges.codewars;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class MmultiplesN {
 
   public static void main(String[] args) {
     int[] res = multiples(3, 5);
+    Arrays.stream(res).map(v -> v*3).sum();
     System.out.println(Arrays.toString(res));
   }
 
@@ -15,6 +17,8 @@ public class MmultiplesN {
     for (int i = 0; i < m; i++) {
       results[i] = n*(i+1);
     }
-    return results;
+
+    // Better way using IntStrem
+    return IntStream.rangeClosed(1, m).map(v -> v*n).toArray();
   }
 }
