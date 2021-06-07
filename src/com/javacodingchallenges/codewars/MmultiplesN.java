3b42833923188de504558d9c8828fcee32872923
jbozarov <tech@jbozarov.com>
2021-06-07 14:46:42 -0400

    Mulitples M and N finished

--- src/com/javacodingchallenges/codewars/MmultiplesN.java
+++ src/com/javacodingchallenges/codewars/MmultiplesN.java
@@ -1,4 +1,20 @@
 package com.javacodingchallenges.codewars;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MmultiplesN {
+
+  public static void main(String[] args) {
+    int[] res = multiples(3, 5);
+    System.out.println(Arrays.toString(res));
+  }
+
+  public static int[] multiples(int m, int n) {
+    int[] results = new int[m];
+    for (int i = 0; i < m; i++) {
+      results[i] = n*(i+1);
+    }
+    return results;
+  }
 }
