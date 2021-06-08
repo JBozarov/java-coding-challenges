840c963075ffd822c8546802cd01221cb8ac8774
jbozarov <tech@jbozarov.com>
2021-06-08 14:52:52 -0400

    added OddOrEven challenge

---
+++ src/com/javacodingchallenges/codewars/OddOrEven.java
@@ -0,0 +1,18 @@
+package com.javacodingchallenges.codewars;
+
+import java.util.Arrays;
+
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 34, 6};
+        int[] numOne = {0};
+        System.out.println(oddOrEven(nums));
+        System.out.println(oddOrEven(numOne));
+    }
+
+    public static String oddOrEven (int[] array) {
+        return Arrays.stream(array).reduce(0, (a, b) -> a + b)%2 == 0 ? "Even" : "Odd";
+    }
+}
