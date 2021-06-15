cfbbc8f5706bc35b1944b0f69f7f0c63c4d2f45c
jbozarov <tech@jbozarov.com>
2021-06-15 13:28:14 -0400

    TheSupermarketQueue challenge, optimized, removed unneeded code

---
+++ src/com/javacodingchallenges/codewars/TakeATenMinuteWalk.java
@@ -0,0 +1,30 @@
+package com.javacodingchallenges.codewars;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TakeATenMinuteWalk {
+  public static void main(String[] args) {
+      char[] directions = new char[] {'n','s','n','s','n','s','n','s','n','s', 'e'};
+      System.out.println(isValid(directions));
+  }
+
+  public static boolean isValid(char[] walk) {
+    Map<Character, Integer> directionsMap = new HashMap<>();
+        for (char c : walk) {
+          if (directionsMap.containsKey(c)) {
+            directionsMap.replace(c, directionsMap.get(c) + 1);
+          }
+          else {
+            directionsMap.put(c, 1);
+          }
+        }
+      List<Character> keysList = new ArrayList<>(directionsMap.keySet());
+    System.out.println();
+    if (keysList.size() == 1 || keysList.size() == 3) {
+        return false;
+    } // else if (keysList.size() == 2 && ())
+
+    return true;
+  }
+}
