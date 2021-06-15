2bbf4d19c0eeda0bfb8a171cfc951608db777856
jbozarov <tech@jbozarov.com>
2021-06-15 13:29:16 -0400

    TheSupermarketQueue challenge, optimized, removed unneeded code

--- src/com/javacodingchallenges/codewars/TheSupermarketQueue.java
+++ src/com/javacodingchallenges/codewars/TheSupermarketQueue.java
@@ -1,25 +1,15 @@
 package com.javacodingchallenges.codewars;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class TheSupermarketQueue {
   public static void main(String[] args) {
     int[] queue = {10, 2, 14, 5};
-    System.out.println(solveSuperMarketQueue(queue, 23));
+    System.out.println(solveSuperMarketQueue(queue, 2)); // 16
+    System.out.println(solveSuperMarketQueue(queue, 20)); // 14
   }
 
   public static int solveSuperMarketQueue(int[] customers, int n) {
-//    int max = 0;
-//    for (int i : customers) {
-//      if (max < i) {
-//        max = i;
-//      }
-//    }
-//    if (n >= customers.length) {
-//      return max;
-//    }
-
     Map<Integer, Integer> isles = new HashMap<>();
     for (int i = 1; i <= n; i++) {
       isles.put(i, 0);
@@ -28,9 +18,7 @@ public class TheSupermarketQueue {
     for (int customer : customers) {
       Integer minValueOfMap = isles.entrySet().stream().min(Map.Entry.comparingByValue()).get().getValue();
       Integer keyOfMinValueOfMap = isles.entrySet().stream().min(Map.Entry.comparingByValue()).get().getKey();
-      System.out.println("pair is " + keyOfMinValueOfMap + " " +minValueOfMap);
       isles.replace(keyOfMinValueOfMap, minValueOfMap + customer);
-      System.out.println("map is " + isles);
     }
 
     return isles.entrySet().stream().max(Map.Entry.comparingByValue()).get().getValue();
