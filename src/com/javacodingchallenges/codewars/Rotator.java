aa975150689876465ffbe098d2363d66bf5cf962
jbozarov <tech@jbozarov.com>
2021-06-18 14:31:42 -0400

    Rotator challenge started

---
+++ src/com/javacodingchallenges/codewars/Rotator.java
@@ -0,0 +1,19 @@
+package com.javacodingchallenges.codewars;
+
+public class Rotator {
+  public static void main(String[] args) {
+    //
+  }
+
+    public Object[] rotate(Object[] data, int n) {
+      if (n == 0) {
+          return data;
+      }
+      else if (n>0) {
+          for (Object d : data) {
+
+          }
+      }
+       return new String[]{"Hell", "DKF"};
+    }
+}
