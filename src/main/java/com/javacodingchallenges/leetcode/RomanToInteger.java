ddc0426bed859e13ab1fbcd7874b3d9d849eecc8
jbozarov <tech@jbozarov.com>
2021-09-19 10:55:06 -0400

    Code optimized

--- src/main/java/com/javacodingchallenges/leetcode/RomanToInteger.java
+++ src/main/java/com/javacodingchallenges/leetcode/RomanToInteger.java
@@ -5,14 +5,10 @@ import java.util.TreeMap;
 
 public class RomanToInteger {
     public static void main(String[] args) {
-        System.out.println(romanToInt("XXVII"));
-        System.out.println("--------------------------------------------");
-        System.out.println(romanToInt("XXIV"));
-        System.out.println("--------------------------------------------");
-        System.out.println(romanToInt("MCMXCIV"));
-        System.out.println("--------------------------------------------");
-        System.out.println(romanToInt("VIII"));
-        System.out.println("--------------------------------------------");
+        System.out.println(romanToInt("XXVII")); // 27
+        System.out.println(romanToInt("XXIV")); // 24
+        System.out.println(romanToInt("MCMXCIV")); // 1994
+        System.out.println(romanToInt("VIII")); // 8
         System.out.println(romanToInt("MCMXCVI")); // 1996
     }
 
@@ -29,7 +25,6 @@ public class RomanToInteger {
         int result = 0;
         int count = 1;
         for (int i = 0; i < s.length(); i++ ) {
-            System.out.println(" count-" + count + " i-" + i + " s.length()-" + s.length());
             if (count < s.length() && i < s.length() - 1) {
                 if ( romanValues.get(s.charAt(i)) < romanValues.get(s.charAt(i+1)) ) {
                   result += romanValues.get(s.charAt(i+1)) - romanValues.get(s.charAt(i));
@@ -41,7 +36,6 @@ public class RomanToInteger {
                 result += romanValues.get(s.charAt(i));
             }
             count ++;
-            System.out.println(" result is " + result);
         }
 
         return result;
