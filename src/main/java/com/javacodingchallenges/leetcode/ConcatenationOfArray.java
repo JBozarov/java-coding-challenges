1e115f131e5c8d48c71da157f5de056b589df8a3
jbozarov <tech@jbozarov.com>
2021-09-24 07:43:08 -0400

    Added ConcatenationOfArray challenge

---
+++ src/main/java/com/javacodingchallenges/leetcode/ConcatenationOfArray.java
@@ -0,0 +1,23 @@
+package com.javacodingchallenges.leetcode;
+
+public class ConcatenationOfArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        getConcatenation(nums); // [1, 2, 1, 1, 2, 1]
+
+        int[] nums2 = {1, 3, 2, 1};
+        getConcatenation(nums2); // [1, 3, 2, 1, 1, 3, 2, 1]
+    }
+
+    private static int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+        for (int i = 0; i < nums.length * 2; i ++ ) {
+            int index = i;
+            if ( i >= nums.length ) {
+                index = i - nums.length;
+            }
+            result[i] = nums[index];
+        }
+        return result;
+    }
+}
