f4829dae17259864d63c6744ffa7794ec844cca3
jbozarov <tech@jbozarov.com>
2021-09-27 21:53:28 -0400

    Added LongestCommonPrefix challenge finished

--- src/main/java/com/javacodingchallenges/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/javacodingchallenges/leetcode/LongestCommonPrefix.java
@@ -1,29 +1,35 @@
 package com.javacodingchallenges.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         String[] strs = {"flower", "flow", "flight"};
         System.out.println(longestCommonPrefix(strs));
+
+        String[] sec = {"cir", "car"};
+        System.out.println(longestCommonPrefix(sec));
     }
 
     private static String longestCommonPrefix(String[] strs) {
-
-        String shortest = Arrays.asList(strs)
+        int shortestWordLength = Arrays.asList(strs)
                 .stream()
                 .sorted(( v1, v2 ) -> v1.length() > v2.length() ? 1 : -1)
                 .findFirst()
-                .get();
+                .get().length();
 
-        for (int i = 0; i < shortest.length(); i ++ ) {
+        StringBuilder result = new StringBuilder("");
+        for (int i = 0; i < shortestWordLength; i ++ ) {
+            Set<Character> characterSet = new HashSet<>();
             for (String s : strs) {
-                char c = s.charAt(i);
-                System.out.println(s);
+                characterSet.add(s.charAt(i));
+            }
+            if (characterSet.size() == 1) {
+                result.append(strs[0].charAt(i));
+            } else {
+                break;
             }
         }
-
-
-        return shortest;
+        return result.toString();
     }
 }
