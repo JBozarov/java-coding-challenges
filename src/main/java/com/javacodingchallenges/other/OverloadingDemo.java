d5af392c8267166cd11c046bdd9124fdfef1f3e8
jbozarov <tech@jbozarov.com>
2021-12-13 23:30:11 -0500

    Overloading finished

--- src/main/java/com/javacodingchallenges/other/OverloadingDemo.java
+++ src/main/java/com/javacodingchallenges/other/OverloadingDemo.java
@@ -1,2 +1,17 @@
-package com.javacodingchallenges.other;public class OverloadingDemo {
+package com.javacodingchallenges.other;
+
+public class OverloadingDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        callMe(i);  // long is i, why ?
+    }
+
+
+    public static void callMe(Long l) {
+        System.out.println("long is " + l);
+    }
+
+    public static void callMe(int i) {
+        System.out.println("long is " + i);
+    }
 }
