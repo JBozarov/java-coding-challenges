a3d1155aa1f17ce8bf9d2ed983eea4c78039f638
jbozarov <tech@jbozarov.com>
2021-12-25 11:25:02 -0500

    Bubble sort changes

---
+++ src/main/java/com/javacodingchallenges/algorithms/recursion/StringDemo.java
@@ -0,0 +1,2 @@
+package com.javacodingchallenges.algorithms.recursion;public class StringDemo {
+}
--- src/main/java/com/javacodingchallenges/algorithms/sorting/BubbleSort.java
+++ src/main/java/com/javacodingchallenges/algorithms/sorting/BubbleSort.java
@@ -1,5 +1,8 @@
 package com.javacodingchallenges.algorithms.sorting;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +14,9 @@ public class BubbleSort {
         List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 23, 4, 5, 2, 5, 98, 63, 51));
         int[] ints = {1, 2, 23, 4, 15, 21, 5, 98, 63, 51};
 
-
         for (int i = 0; i < ints.length -1; i ++ ) {
             for (int k = 0; k < ints.length - i - 1; k++ ) {
+                System.out.println("i " + i + " k " + k);
                 if (ints[k] > ints[k + 1]) {
                     int temp = ints[k];
                     ints[k] = ints[k+1];
@@ -24,6 +27,16 @@ public class BubbleSort {
         }
 
         System.out.println(Arrays.toString(ints));
+
+        List myList = new ArrayList();
+        myList.add(12);
+        myList.add("Hello");
+        myList.add(true);
+
+
+        NodeList nodeList = new ConcreteList();
+        nodeList.getLength();
+        System.out.println(nodeList.getLength());
 
     }
 }
