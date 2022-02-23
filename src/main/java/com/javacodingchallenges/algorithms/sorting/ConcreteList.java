650ab192bc0621fe9bc841fe674979a5b472d3ac
jbozarov <tech@jbozarov.com>
2022-02-22 20:28:45 -0500

    ConcreteList added

---
+++ src/main/java/com/javacodingchallenges/algorithms/sorting/ConcreteList.java
@@ -0,0 +1,17 @@
+package com.javacodingchallenges.algorithms.sorting;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ConcreteList implements NodeList {
+
+    @Override
+    public Node item(int index) {
+        return this.item(index);
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+}
