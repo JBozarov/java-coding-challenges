4324d39fd0b8779c2fbef98ad8f6dc0e65e3c5ae
jbozarov <tech@jbozarov.com>
2022-06-11 08:49:08 -0400

    Dog added

--- src/main/java/com/javacodingchallenges/test/Dog.java
+++ src/main/java/com/javacodingchallenges/test/Dog.java
@@ -1,2 +1,13 @@
-package com.javacodingchallenges.test;public class Dog {
+package com.javacodingchallenges.test;
+
+public class Dog extends Animal {
+    public void run(){
+        System.out.println("Dog is running");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.run();
+        dog.run("HEllO");
+    }
 }
