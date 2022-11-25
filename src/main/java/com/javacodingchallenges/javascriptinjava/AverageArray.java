160951460667b0d6e82abc7185ff19ebb448f81c
jbozarov <tech@jbozarov.com>
2022-11-25 14:02:40 -0500

    AverageArray added

--- src/main/java/com/javacodingchallenges/javascriptinjava/AverageArray.java
+++ src/main/java/com/javacodingchallenges/javascriptinjava/AverageArray.java
@@ -1,2 +1,20 @@
-package com.javacodingchallenges.javascriptinjava;public class AverageArray {
+package com.javacodingchallenges.javascriptinjava;
+
+import java.util.Arrays;
+
+public class AverageArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {5, 6, 7, 8};
+        int[] average = averageArr(arr1, arr2);
+        System.out.println(Arrays.toString(average));
+    }
+
+    private static int[] averageArr(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length];
+        for ( int i = 0; i < arr1.length; i++ ) {
+            result[i] = ( arr1[i] + arr2[i] ) / 2;
+        }
+        return result;
+    }
 }
