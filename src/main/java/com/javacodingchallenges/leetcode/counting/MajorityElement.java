4b8808c79f82a22b2f8d60e08e13d22c02bfcd98
jbozarov <tech@jbozarov.com>
2023-02-03 15:02:36 -0500

    MajorityElement challenge solved

---
+++ src/main/java/com/javacodingchallenges/leetcode/counting/MajorityElement.java
@@ -0,0 +1,20 @@
+package com.javacodingchallenges.leetcode.counting;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 3};
+        System.out.println(majorityElement(arr)); // 3
+
+        int[] arr2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(arr2)); // 2
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        int majorityElement = 0;
+
+
+
+        return majorityElement;
+    }
+}
