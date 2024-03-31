f3cbd44be48f0d2c5f9572969ffd357b3f8e4a8e
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2024-03-31 08:01:25 -0400

    Update FindFirstPalindromeStringInArray.java

--- src/main/java/com/javacodingchallenges/leetcode/string/FindFirstPalindromeStringInArray.java
+++ src/main/java/com/javacodingchallenges/leetcode/string/FindFirstPalindromeStringInArray.java
@@ -1,1 +1,16 @@
-sdfsdfsdf
+
+
+public class FindFirstPalindromeStringInArray {
+
+// Leetcode 2108
+  public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder(word); 
+            String reversed = sb.reverse().toString(); 
+            if (word.equals(reversed)) {
+                return word; 
+            }
+        }
+        return ""; 
+    }
+}
