4994a852df5f382c56f9a6bdea82bbb6571c5ee2
jahongir <tech@jbozarov.com>
2024-04-06 22:45:44 -0400

    Count Primes Leetcode

--- src/main/java/com/javacodingchallenges/leetcode/math/CountPrimes.java
+++ src/main/java/com/javacodingchallenges/leetcode/math/CountPrimes.java
@@ -2,11 +2,13 @@ package com.javacodingchallenges.leetcode.math;
 
 public class CountPrimes {
     public static void main(String[] args) {
-//        System.out.println(countPrimes(0)); // 0
-//        System.out.println(countPrimes(1)); // 0
-//        System.out.println(countPrimes(2)); // 1
+//        System.out.println(countPrimes(0));   // 0
+//        System.out.println(countPrimes(1));   // 0
+//        System.out.println(countPrimes(2));   // 0
 //        System.out.println(countPrimes(-90)); // 0
-        System.out.println(countPrimes(10)); // 4
+        System.out.println(countPrimes(10));  // 4
+        System.out.println(countPrimes(20));  // 8
+        System.out.println(countPrimes(30));  // 10
     }
 
 
@@ -18,17 +20,16 @@ public class CountPrimes {
         } else if (n == 3) {
             return 1;
         } else {
-            int startNumber = 5;
-            for ( int i = 3; i < n; i += 2 ) {
-                for (int k = 3; k < i; k++) {
-
-                    if (i % k != 0) {
-                        System.out.println("true " + i + " and k " + k);
-                        count++;
-                        break;
+            for (int i = 5; i < n; i += 2) {
+                int tempCount = 0;
+                for (int k = 2; k <= i; k++) {
+                    if (i % k == 0) {
+                        tempCount++;
                     }
                 }
-
+                if (tempCount > 0) {
+                    count++;
+                }
             }
 
 
