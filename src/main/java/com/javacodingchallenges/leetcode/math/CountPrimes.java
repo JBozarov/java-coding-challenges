d3b47e78816e74f6335fc69378cbbaa9a08cb70f
jahongir <tech@jbozarov.com>
2024-04-06 22:58:45 -0400

    CountPrime finished !

--- src/main/java/com/javacodingchallenges/leetcode/math/CountPrimes.java
+++ src/main/java/com/javacodingchallenges/leetcode/math/CountPrimes.java
@@ -2,10 +2,10 @@ package com.javacodingchallenges.leetcode.math;
 
 public class CountPrimes {
     public static void main(String[] args) {
-//        System.out.println(countPrimes(0));   // 0
-//        System.out.println(countPrimes(1));   // 0
-//        System.out.println(countPrimes(2));   // 0
-//        System.out.println(countPrimes(-90)); // 0
+        System.out.println(countPrimes(0));   // 0
+        System.out.println(countPrimes(1));   // 0
+        System.out.println(countPrimes(2));   // 0
+        System.out.println(countPrimes(-90)); // 0
         System.out.println(countPrimes(10));  // 4
         System.out.println(countPrimes(20));  // 8
         System.out.println(countPrimes(30));  // 10
@@ -13,8 +13,7 @@ public class CountPrimes {
 
 
     public static int countPrimes (int n) {
-        int count = 1;
-
+        int count = 2;
         if (n <= 2) {
             return 0;
         } else if (n == 3) {
@@ -22,19 +21,16 @@ public class CountPrimes {
         } else {
             for (int i = 5; i < n; i += 2) {
                 int tempCount = 0;
-                for (int k = 2; k <= i; k++) {
+                for (int k = 1; k <= i; k++) {
                     if (i % k == 0) {
                         tempCount++;
                     }
                 }
-                if (tempCount > 0) {
+                if (tempCount == 2) {
                     count++;
                 }
             }
-
-
         }
-
         return count;
     }
 }
