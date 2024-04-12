59b96e6545d44214416cf9073efea7ef6cd8a0f7
jahongir <tech@jbozarov.com>
2024-04-11 21:52:23 -0400

    progress

--- src/main/java/com/javacodingchallenges/leetcode/hash_table_sorting/MaximumNumberOfWordsYouCanType.java
+++ src/main/java/com/javacodingchallenges/leetcode/hash_table_sorting/MaximumNumberOfWordsYouCanType.java
@@ -3,5 +3,22 @@ package com.javacodingchallenges.leetcode.hash_table_sorting;
 public class MaximumNumberOfWordsYouCanType {
     public static void main(String[] args) {
 
+        System.out.println(canBeTypedWords("hello world", "ad")); // 1
+        System.out.println(canBeTypedWords("leet code", "lt"));   // 1
+        System.out.println(canBeTypedWords("leet code", "e"));    // 0
+
+    }
+
+
+    public static int canBeTypedWords(String text, String brokenLetters) {
+        int count = 0;
+        for (String word : text.split(" ")) {
+            for (char singleChar : brokenLetters.toCharArray()) {
+                if (!word.contains(String.valueOf(singleChar))) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
