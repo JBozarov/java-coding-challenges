298d08dd000fbe03018d5d9281176bc0cccf0152
jahongir <tech@jbozarov.com>
2024-04-14 09:47:25 -0400

    DivisibleAndNonDivisibleSumsDifference created

---
+++ src/main/java/com/javacodingchallenges/leetcode/math/DivisibleAndNonDivisibleSumsDifference.java
@@ -0,0 +1,19 @@
+package com.javacodingchallenges.leetcode.math;
+
+public class DivisibleAndNonDivisibleSumsDifference {
+
+    public static void main(String[] args) {
+        System.out.println(differenceOfSums(10, 3));  // 19
+    }
+
+
+
+
+    public static int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+
+        return num1 - num2;
+    }
+}
+}
